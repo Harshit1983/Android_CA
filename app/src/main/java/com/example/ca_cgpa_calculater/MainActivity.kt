b3d1ca1b9283f8d16990295a036593e217b047d0
{Harshit} <{harshitpandiyar1983@gmail.com}>
2025-11-19 14:39:03 +0530

    First Commit

---
+++ app/src/main/java/com/example/ca_cgpa_calculater/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ca_cgpa_calculater
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ca_cgpa_calculater.ui.theme.CA_CGPA_CALCULATERTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CA_CGPA_CALCULATERTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CA_CGPA_CALCULATERTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/ca_cgpa_calculater/cgpa_calculater.kt
@@ -0,0 +1,53 @@
+package com.example.ca_cgpa_calculater
+
+import ads_mobile_sdk.s1
+import android.icu.text.DisplayContext
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class cgpa_calculater : ComponentActivity() {
+    lateinit var s1: EditText
+    lateinit var s2: EditText
+    lateinit var s3: EditText
+    lateinit var b: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cgpa_calculater)
+
+        s1 = findViewById(R.id.et1)
+        s2 = findViewById(R.id.et2)
+        s3 = findViewById(R.id.et3)
+        b = findViewById(R.id.b1)
+
+        b.setOnClickListener {
+
+            if (s1.text.isEmpty() || s2.text.isEmpty() || s3.text.isEmpty()) {
+                Toast.makeText(this, "Please enter all values", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val c1 = s1.text.toString().toFloat()
+            val c2 = s2.text.toString().toFloat()
+            val c3 = s3.text.toString().toFloat()
+
+            val cal = c1 + c2 + c3
+
+            Toast.makeText(this, cal.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+
+
+
+
+
